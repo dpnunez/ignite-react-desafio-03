24ad845d1652b84c649b3be5c3a33361cdaf9ae4
Daniel Nunez <daniel.nunez@nave.rs>
2021-07-17 19:42:48 -0300

    feat: fetch shoes list

---
+++ src/helpers/index.ts
@@ -0,0 +1,4 @@
+  
+const formatMoney = (value: number) => new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
+
+export { formatMoney }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import { ProductList } from './styles';
 import { api } from '../../services/api';
 import { formatPrice } from '../../util/format';
 import { useCart } from '../../hooks/useCart';
+import { toast } from 'react-toastify';
+import { formatMoney } from '../../helpers';
 
 interface Product {
   id: number;
@@ -22,7 +24,7 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
   // const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
@@ -31,7 +33,12 @@ const Home = (): JSX.Element => {
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      try {
+				const { data: products } = await api.get("/products")
+				setProducts(products)
+			} catch (e) {
+				toast.error(e.message)
+			}
     }
 
     loadProducts();
@@ -43,23 +50,25 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+			{products.map(product => (
+				<li>
+					<img src={product.image} alt={product.title} />
+					<strong>{product.title}</strong>
+					<span>{formatMoney(product.price)}</span>
+					<button
+						type="button"
+						data-testid="add-product-button"
+					// onClick={() => handleAddProduct(product.id)}
+					>
+						<div data-testid="cart-product-quantity">
+							<MdAddShoppingCart size={16} color="#FFF" />
+							{/* {cartItemsAmount[product.id] || 0} */} 2
+						</div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+						<span>ADICIONAR AO CARRINHO</span>
+					</button>
+				</li>
+			))}
     </ProductList>
   );
 };
